34e1cb97762e7f2f41032463998d481aea6b230e
NAVAMI JYOTHSNA <navamijyothsna@gmail.com>
2026-02-22 12:15:55 +0530

    added isro satellite page

--- weather/urls.py
+++ weather/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('satellites/', views.satellites, name='satellites'),
 ]
+
--- weather/views.py
+++ weather/views.py
@@ -32,3 +32,29 @@ def home(request):
                 error = f"Could not fetch weather for {city}"
 
     return render(request, "weather/home.html", {"weather_list": weather_list, "error": error})
+
+from django.shortcuts import render
+
+def satellites(request):
+    satellite_data = [
+        {
+            "name": "INSAT-3D",
+            "launch": "2013",
+            "purpose": "Weather Monitoring",
+            "orbit": "Geostationary",
+        },
+        {
+            "name": "Cartosat-3",
+            "launch": "2019",
+            "purpose": "High Resolution Imaging",
+            "orbit": "Sun-Synchronous",
+        },
+        {
+            "name": "Oceansat-2",
+            "launch": "2009",
+            "purpose": "Ocean Surface Study",
+            "orbit": "Polar",
+        },
+    ]
+
+    return render(request, "satellites.html", {"satellites": satellite_data})
